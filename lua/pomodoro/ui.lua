c8153496ebd666575655f361b21e7e61cc68f648
James Teague <james@teague.dev>
2024-08-01 12:13:09 -0500

    remove buffer event that stops timer

--- lua/pomodoro/ui.lua
+++ lua/pomodoro/ui.lua
@@ -10,32 +10,36 @@ function ui.pomodoro_completed_menu(pomodoro)
 		pomodoro.opts.ui.border.text.top = "[Pomodoro Completed]"
 	end
 
-  local menu_options = {
-    keymap = pomodoro.opts.keymap,
-    lines = { Menu.item('Take break'), Menu.item('Quit') },
-    on_close = function () pomodoro:stop() end,
-    on_submit = function(item)
-      if item.text == 'Quit' then
-        pomodoro:stop()
-      else
-        pomodoro:start_break()
-      end
-    end
-  }
-  local menu = Menu(popup_options, menu_options)
-  menu:mount()
-  menu:on(event.BufLeave, function()
-    pomodoro:stop()
-    menu:unmount()
-  end, { once = true })
-  menu:map('n', 'b', function()
-    pomodoro:start_break()
-    menu:unmount()
-  end, { noremap = true })
-  menu:map('n', 'q', function()
-    pomodoro:stop()
-    menu:unmount()
-  end, { noremap = true })
+	local menu_options = {
+		keymap = pomodoro.opts.keymap,
+		lines = { Menu.item("Take break"), Menu.item("Quit") },
+		on_close = function()
+			pomodoro:stop()
+		end,
+		on_submit = function(item)
+			if item.text == "Quit" then
+				pomodoro:stop()
+			else
+				pomodoro:start_break()
+			end
+		end,
+	}
+	local menu = Menu(popup_options, menu_options)
+	menu:mount()
+	-- NOTE: This was removed because it was executing even when an item was chosen
+	-- or mapped key was pressed
+	-- menu:on(event.BufLeave, function()
+	-- 	pomodoro:stop()
+	-- 	menu:unmount()
+	-- end, { once = true })
+	menu:map("n", "b", function()
+		pomodoro:start_break()
+		menu:unmount()
+	end, { noremap = true })
+	menu:map("n", "q", function()
+		pomodoro:stop()
+		menu:unmount()
+	end, { noremap = true })
 end
 
 ---@param pomodoro Pomodoro
@@ -58,20 +62,22 @@ function ui.break_completed_menu(pomodoro)
     end
   }
 
-  local menu = Menu(popup_options, menu_options)
-  menu:mount()
-  menu:on(event.BufLeave, function()
-    pomodoro:stop()
-    menu:unmount()
-  end, { once = true })
-  menu:map('n', 'p', function()
-    pomodoro:start_pomodoro()
-    menu:unmount()
-  end, { noremap = true })
-  menu:map('n', 'q', function()
-    pomodoro:stop()
-    menu:unmount()
-  end, { noremap = true })
+	local menu = Menu(popup_options, menu_options)
+	menu:mount()
+	-- NOTE: This was removed because it was executing even when an item was chosen
+	-- or mapped key was pressed
+	-- menu:on(event.BufLeave, function()
+	-- 	pomodoro:stop()
+	-- 	menu:unmount()
+	-- end, { once = true })
+	menu:map("n", "p", function()
+		pomodoro:start_pomodoro()
+		menu:unmount()
+	end, { noremap = true })
+	menu:map("n", "q", function()
+		pomodoro:stop()
+		menu:unmount()
+	end, { noremap = true })
 end
 
 return ui
